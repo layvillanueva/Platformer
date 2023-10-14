20e35f90d4c4ed09146c2d9cd7b53210b48bf915
Nate Brevin Que <nate.que@obf.ateneo.edu>
2023-10-14 20:36:29 +0800

    Initial functionality (issue: collision detection)

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float x;
+    private float width;
+    private float y;
+    private float height;
+
+    private PlayerController player;
+
+    void Start()
+    {
+        Transform objTransform = this.gameObject.transform;
+
+        x = objTransform.position.x;
+        width = objTransform.localScale.x;
+
+        y = objTransform.position.y;
+        height = objTransform.localScale.y;
+
+        player = (PlayerController)FindObjectOfType(typeof(PlayerController));
+    }
+
+    void Update()
+    {
+        if(CollidingWith(player.gameObject.transform)){
+            player.IsCollidingWithObject();
+        }
+    }
+
+    bool CollidingWith(Transform other)
+    {
+        Vector3 otherPosition = other.position;
+        Vector3 otherScale = other.localScale;
+
+        return ((x + width / 2 > otherPosition.x - otherScale.x / 2) &&
+                (otherPosition.x + otherScale.x / 2 > x - width / 2) &&
+                (y + height / 2 > otherPosition.y - otherScale.y / 2) &&
+                (otherPosition.y + otherScale.y / 2 > y - height / 2));
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform objTransform;
+
+    public float horizV;
+    private float maxHorizV;
+    public float horizA;
+    private float x;
+
+    private bool isJumping;
+    public float vertV;
+    private float jumpHeight;
+    private float gravity;
+    private float y;
+
+    void Start()
+    {
+        objTransform = this.gameObject.transform;
+
+        horizV = 0;
+        maxHorizV = 6;
+        horizA = 0;
+        x = 0;
+
+        isJumping = false;
+        vertV = 0;
+        jumpHeight = 2.5f;
+        gravity = -6;
+        y = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            if (horizV > 0){
+                horizA = 0;
+                horizV = 0;
+            }
+
+            if (horizV < -maxHorizV){
+                horizA = 0;
+                horizV = -maxHorizV;
+            } else {
+                horizA = horizA - 8 * Time.deltaTime;
+            }
+
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            if (horizV < 0){
+                horizA = 0;
+                horizV = 0;
+            } 
+
+            if (horizV > maxHorizV){
+                horizA = 0;
+                horizV = maxHorizV;
+            } else {
+                horizA = horizA + 8 * Time.deltaTime;
+            }
+
+        } else {
+            horizA = horizA - (horizV * 16) * Time.deltaTime;
+
+            if (Mathf.Round(horizV) == 0){
+                horizA = 0;
+                horizV = 0;
+            }
+        }
+
+        horizV = horizV + horizA * Time.deltaTime;
+        x = x + horizV * Time.deltaTime;
+
+
+        if (!isJumping){
+            if (Input.GetKeyDown(KeyCode.Space)){
+                // formula from unity documentation
+                vertV += Mathf.Sqrt(jumpHeight * -3 * gravity);
+
+                isJumping = true;
+            }
+        }
+
+        vertV = vertV + gravity * Time.deltaTime;
+        y = y + vertV * Time.deltaTime;
+
+        objTransform.position = new Vector3(x, y, 0);
+    }
+
+    public void IsCollidingWithObject()
+    {
+        if (horizV > 0){
+            horizA = 0;
+            horizV = 0;
+            x -= 0.01f;
+        } else if (horizV < 0){
+            horizA = 0;
+            horizV = 0;
+            x += 0.01f;            
+        }
+
+        if (vertV > 0){
+            vertV = 0;
+        } else if (vertV < 0){
+            vertV = 0;
+            y += 0.01f;
+            isJumping = false;
+        }
+    }
+}
